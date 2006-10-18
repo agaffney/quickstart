1c136e8bd2ce3468dee4d0ea940ee8e299699a4b
agaffney <agaffney@ff364d23-de1e-0410-b81d-d977a1fe94ee>
2006-10-18 15:01:36 +0000

    change drive size buffer to 2 instead of 15

--- modules/partition.sh
+++ modules/partition.sh
@@ -3,7 +3,7 @@
 get_device_size_in_mb() {
   local device=$1
 
-  expr $(expr $(awk "/${device}$/ { print \$3; }" /proc/partitions) / 1024) - 15 # just to make sure we don't go past the end of the drive
+  expr $(expr $(awk "/${device}$/ { print \$3; }" /proc/partitions) / 1024) - 2 # just to make sure we don't go past the end of the drive
 }
 
 human_size_to_mb() {
