c035bbe604801fe3487b7b08302869f12e5eb38e
agaffney <agaffney@ff364d23-de1e-0410-b81d-d977a1fe94ee>
2006-12-05 18:52:30 +0000

    fix error checking

--- modules/bootloader_x86.sh
+++ modules/bootloader_x86.sh
@@ -21,7 +21,10 @@ configure_bootloader_grub() {
     local kv="$(echo ${kernel} | sed -e 's:^kernel-genkernel-[^-]\+-::')"
     echo "title=Gentoo Linux ${kv}" >> ${chroot_dir}/boot/grub/grub.conf
     local grub_device="$(map_device_to_grub_device ${boot_device})"
-    [ -z "${grub_device}" ] && { error "could not map boot device ${boot_device} to grub device" && return 1 }
+    if [ -z "${grub_device}" ]; then
+      error "could not map boot device ${boot_device} to grub device"
+      return 1
+    fi
     echo -en "root (${grub_device},$(expr ${boot_minor} - 1))\nkernel /boot/${kernel} " >> ${chroot_dir}/boot/grub/grub.conf
     if [ -z "${initrd}" ]; then
       echo "root=${root}" >> ${chroot_dir}/boot/grub/grub.conf
@@ -30,8 +33,14 @@ configure_bootloader_grub() {
       echo -e "initrd /boot/${initrd}\n" >> ${chroot_dir}/boot/grub/grub.conf
     fi
   done
-  spawn_chroot "grep -v rootfs /proc/mounts > /etc/mtab" || { error "could not copy /proc/mounts to /etc/mtab" && return 1 }
+  if ! spawn_chroot "grep -v rootfs /proc/mounts > /etc/mtab"; then
+    error "could not copy /proc/mounts to /etc/mtab"
+    return 1
+  fi
   [ -z "${bootloader_install_device}" ] && bootloader_install_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
-  spawn_chroot "grub-install ${bootloader_install_device}" || { error "could not install grub to ${bootloader_install_device}" && return 1 }
+  if ! spawn_chroot "grub-install ${bootloader_install_device}"; then
+    error "could not install grub to ${bootloader_install_device}"
+    return 1
+  fi
 }
 
