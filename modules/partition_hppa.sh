4006b722a1e05b6810057885e97253c27aca672a
agaffney <agaffney@ff364d23-de1e-0410-b81d-d977a1fe94ee>
2006-12-21 05:24:34 +0000

    add sanity check functions to partition_{hppa,amd64}

--- modules/partition_hppa.sh
+++ modules/partition_hppa.sh
@@ -1,3 +1,7 @@
 # $Id$
 
 import partition_x86
+
+sanity_check_config_partition() {
+  warn "Sanity checking partition config for hppa"
+}
--- modules/partition_x86.sh
+++ modules/partition_x86.sh
@@ -1,7 +1,5 @@
 # $Id$
 
-unset sanity_check_config_partition
-
 sanity_check_config_partition() {
   warn "Sanity checking partition config for x86"
 }
