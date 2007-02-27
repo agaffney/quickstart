e9051c8c0e0dc5894fa8c303a9bd140b297cea27
agaffney <agaffney@ff364d23-de1e-0410-b81d-d977a1fe94ee>
2007-02-27 14:41:27 +0000

    add detect_disks() function. inspired by code from Frank Zwart <frank@kanariepietje.nl>

--- modules/misc.sh
+++ modules/misc.sh
@@ -4,3 +4,16 @@ get_arch() {
   uname -m | sed -e 's:i[3-6]86:x86:' -e 's:x86_64:amd64:' -e 's:parisc:hppa:'
 }
 
+detect_disks() {
+  if [ ! -d "/sys" ]; then
+    error "Cannot detect disks due to missing /sys"
+    exit 1
+  fi
+  count=0
+  for i in /sys/block/[hs]d[a-z]; do
+    if [ "$(< ${i}/removable)" = "0" ]; then
+      eval "disk${count}=$(basename ${i})"
+      count=$(expr ${count} + 1)
+    fi
+  done
+}
