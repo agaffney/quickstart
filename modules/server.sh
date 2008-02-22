45288b14c5cf63e422db0efcf586ce284a084d49
agaffney <agaffney@ff364d23-de1e-0410-b81d-d977a1fe94ee>
2008-02-22 01:30:53 +0000

    initial implementation of quickstart server protocol handler

---
+++ modules/server.sh
@@ -0,0 +1,36 @@
+# $Id$
+
+server_init() {
+  if [ -n "$(echo ${server} | grep :)" ]; then
+    server_host=$(echo "${server}" | cut -d : -f 1)
+    server_port=$(echo "${server}" | cut -d : -f 2)
+  else
+    server_host=${server}
+  fi
+  mac_address=$(get_mac_address)
+  if [ -z "${server_port}" ]; then
+    server_port=8899
+  fi
+}
+
+server_send_request() {
+  local command=$1
+  local args=$2
+
+  fetch "quickstart:///${command}?${args}" "/tmp/server_response"
+  cat /tmp/server_response
+}
+
+server_get_profile() {
+  local profile_uri=$(server_send_request "get_profile_path" "mac=${mac_address}")
+  debug server_get_profile "profile URI is ${profile_uri}"
+  if ! fetch "${profile_uri}" "/tmp/quickstart_profile"; then
+    error "could not fetch profile"
+    exit 1
+  fi
+  notify "fetched profile from ${profile_uri}"
+}
+
+pre_failure_cleanup() {
+  warn "Scire integration doesn't work properly yet!"
+}
