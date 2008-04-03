73b766b8ed08ea1b353b372572c79561c91ff01d
agaffney <agaffney@ff364d23-de1e-0410-b81d-d977a1fe94ee>
2008-04-03 23:04:14 +0000

    apply patch from johnny to use linux32 when calling uname

--- modules/misc.sh
+++ modules/misc.sh
@@ -1,7 +1,7 @@
 # $Id$
 
 get_arch() {
-  uname -m | sed -e 's:i[3-6]86:x86:' -e 's:x86_64:amd64:' -e 's:parisc:hppa:'
+  ${linux32} uname -m | sed -e 's:i[3-6]86:x86:' -e 's:x86_64:amd64:' -e 's:parisc:hppa:'
 }
 
 detect_disks() {
